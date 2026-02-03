4947fd52613bb5c7341d4eec70c8999c3468eb4a
Diyorbek2006 <xolmurotovdiyorbek457@gmail.com>
2026-02-03 14:17:13 +0500

    ify

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import requests
+import json
 
 
 def get_randomuser_full_data():
     url = 'https://randomuser.me/api/'
-
-    # response = requests.get(url)
-    # data = response.json()
-
-    # print(data['results'][0]['name'])
+    response = requests.get(url)
+    return response.json()
 
 
-def get_user_data(user: dict):
-    pass
+def get_user_data():
+    data = get_randomuser_full_data()
+    user = data["results"][0]
+
+    return {
+        "full_name": user["name"]["first"] + " " + user["name"]["last"],
+        "phone": user["phone"],
+        "email": user["email"],
+        "age": user["dob"]["age"],
+        "nat": user["nat"],
+        "gender": user["gender"],
+        "country": user["location"]["country"]
+    }
 
 
-def main() -> None:
-    pass
+def main():
+    result = get_user_data()
+
+    with open("users.json", "w", encoding="utf-8") as f:
+        json.dump(result, f, indent=4)
+
+
 
 main()
